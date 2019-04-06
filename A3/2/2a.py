9de654741c29d12d3879e62c5f7df553b07b35fe
Manish Tanwar <manish.tanwar19@gmail.com>
2019-04-07 03:28:35 +0530

    new done experimenting

---
+++ A3/2/2a.py
@@ -0,0 +1,23 @@
+import sys
+import numpy as np
+from sklearn import preprocessing
+
+file = sys.argv[1]
+out_file = sys.argv[2]
+# start_time = time.time()
+# end_time = time.time()
+# print("Time taken:", end_time - start_time)
+
+input = np.genfromtxt(file,delimiter=',')
+X = input[:,:-1]
+Y = input[:,-1]
+encoder = preprocessing.OneHotEncoder(categories='auto')
+encoder.fit(X)
+labels = encoder.transform(X).toarray()
+Y = Y[:,np.newaxis]
+labels = np.append(labels, Y, axis=1)
+# print(labels)
+print(labels.shape)
+# la = labels[0:20]
+# print(la.shape)
+np.save(out_file, labels)
