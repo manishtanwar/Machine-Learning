38b9271c668a16457c8fc353fbf99c52a3e61db5
Manish Tanwar <manish.tanwar19@gmail.com>
2019-04-18 22:17:44 +0530

    doing pca and svm

---
+++ A4/Hpc/job.sh
@@ -0,0 +1,6 @@
+cd ~
+source ~/.bashrc
+conda activate ml
+cd scratch/ML_A4
+echo $PWD
+python3 pca.py > output.txt
--- A4/Hpc/pca.py
+++ A4/Hpc/pca.py
@@ -2,15 +2,42 @@ import sys
 import glob
 import numpy as np
 from PIL import Image
+from sklearn.decomposition import PCA
 
 # im = Image.open(sys.argv[1]).convert("L")
 # data = np.asarray(im)
 Xpca = []
-Xglo = []
+X = []
+Y = []
+folder_cnt = 0
+for folder in sorted(glob.glob("train_dataset/*")):
+    for img in sorted(glob.glob(folder + "/*.png")):
+        im = Image.open(img).convert("L")
+        data = np.asarray(im).flatten()
+        if(folder_cnt < 50):
+            Xpca.append(data)
+        X.append(data)
+    Y.append(np.genfromtxt(folder + "/rew.csv",delimiter=','))
+    folder_cnt += 1
+    if(folder_cnt == 1):
+        break
 
-for folder in glob.glob("train_dataset/*"):
-    for img in glob.glob(folder):
-        im = Image.open(img)
-        data = np.asarray(im)
+np.save("Xpca",Xpca)
+np.save("X",X)
+np.save("Y",Y)
+print("Xpca.len:",len(Xpca))
+print("X.len:",len(X))
+print("Y.len:",len(Y))
+print("X[0].shape", X[0].shape)
+print("Y[0].shape", Y[0].shape)
+# 849281.hn1.hpc.iitd.ac.in
 
-    print(folder)
+pca = PCA(n_components=50)
+pca.fit(Xpca)
+print(pca.explained_variance_ratio_)
+
+print(X[0].shape)
+
+xn = pca.transform(X)
+print(type(xn), xn[0].shape, xn.shape)
+print(X[0].shape)
